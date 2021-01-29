ad7f2e0b86ab2f5ed815a35f776381b47e3ac41b
wellcardoso <allan.silva@etna.com.br>
2021-01-29 15:02:36 -0300

    trocando o projeto

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.colors.primary}
+`
+
+function index() {
+    return(
+        <Title>My app</Title>
+    )
+}
+
+export default index;
